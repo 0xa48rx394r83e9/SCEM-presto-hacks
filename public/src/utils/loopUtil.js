0db4317eb669b5db9451b453081f2b72e86a4134
0xa48rx394r83e9 <Joseph.paulmer24@stu.dulwich.org>
2021-12-11 17:03:48 +0800

    finishing object boilerplate

---
+++ public/src/utils/loopUtil.js
@@ -0,0 +1,13 @@
+/**
+ * Set loop on a TweenLite tween
+ * must be passed on onComplete
+ *
+ * @method loop
+ */
+ function loop () {
+	/*jshint validthis: true */
+	
+	this.restart();
+  }
+  
+  module.exports = loop;
---
+++ public/src/utils/yoyoUtil.js
@@ -0,0 +1,17 @@
+/**
+ * Set yoyo on a TweenLite tween
+ * must be passed on onComplete and onReverseComplete
+ *
+ * @method yoyo
+ */
+ function yoyo () {
+	/*jshint validthis: true */
+	
+	if (this.reversed()) {
+	  this.restart();
+	} else {
+	  this.reverse();
+	}
+  }
+  
+  module.exports = yoyo;
